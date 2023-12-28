890821fb3785bda7bd0de5fdc80ad7cfb80f58fc
Владимир <113116179+Wloveyou@users.noreply.github.com>
2023-12-28 11:50:57 +0300

    Update 4.3матрицы.cpp

--- 4.3матрицы.cpp
+++ 4.3матрицы.cpp
@@ -1,131 +1,147 @@
-#include <iostream> 
-#include <glm/glm.hpp> 
-using namespace std; 
- 
-// Функция для заполнения матрицы случайными числами 
-void fill_random(glm::mat4& matrix) { 
-    // Инициализируем генератор случайных чисел 
-    srand(time(NULL)); 
-    // Заполняем матрицу случайными числами в диапазоне от 0 до 99 
-    for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 4; j++) { 
-            matrix[i][j] = rand() % 100; 
-        } 
-    } 
-} 
- 
-// Функция для заполнения матрицы с клавиатуры 
-void fill_keyboard(glm::mat4& matrix) { 
-    // Запрашиваем у пользователя ввод элементов матрицы 
-    cout << "Введите элементы матрицы:" << endl; 
-    for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 4; j++) { 
-            cin >> matrix[i][j]; 
-        } 
-    } 
-} 
- 
-// Функция для вывода матрицы на экран 
-void print_matrix(const glm::mat4& matrix) { 
-    // Выводим элементы матрицы в виде таблицы 
-    cout << "Матрица:" << endl; 
-    for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 4; j++) { 
-            cout << matrix[i][j] << "\t"; 
-        } 
-        cout << endl; 
-    } 
-} 
- 
-// Функция для замены максимального элемента каждой строки номером столбца, в котором он находится 
-void replace_max(glm::mat4& matrix) { 
-    // Проходим по каждой строке матрицы 
-    for (int i = 0; i < 4; i++) { 
-        // Инициализируем максимальный элемент и его индекс 
-        float max = matrix[i][0]; 
-        int max_index = 0; 
-        // Ищем максимальный элемент и его индекс в строке 
-        for (int j = 1; j < 4; j++) { 
-            if (matrix[i][j] > max) { 
-                max = matrix[i][j]; 
-                max_index = j; 
-            } 
-        } 
-        // Заменяем максимальный элемент номером столбца 
-        matrix[i][max_index] = max_index; 
-    } 
-} 
- 
-// Функция для вставки после всех строк, содержащих минимальный элемент матрицы, строки 2, 4, 6, ... 
-void insert_row(glm::mat4& matrix) { 
-    // Инициализируем минимальный элемент матрицы 
-    float min = matrix[0][0]; 
-    // Ищем минимальный элемент матрицы 
-    for (int i = 0; i < 4; i++) { 
-        for (int j = 0; j < 4; j++) { 
-            if (matrix[i][j] < min) { 
-                min = matrix[i][j]; 
-            } 
-        } 
-    } 
-    // Считаем количество строк, содержащих минимальный элемент 
-    int count = 0; 
-    for (int i = 0; i < 4; i++) { 
-        bool found = false; 
-        for (int j = 0; j < 4; j++) { 
-            if (matrix[i][j] == min) { 
-                found = true; 
-                break; 
-            } 
-        } 
-        if (found) { 
-            count++; 
-        } 
-    } 
-    // Создаем новую матрицу с увеличенным количеством строк 
-    glm::mat4x3 new_matrix; // Матрица 4x3 
-    // Копируем элементы из старой матрицы в новую, вставляя новые строки 
-    int k = 0; // Индекс для новой матрицы 
-    for (int i = 0; i < 4; i++) { 
-        // Копируем текущую строку из старой матрицы 
-        for (int j = 0; j < 4; j++) { 
-            new_matrix[k][j] = matrix[i][j]; 
-        } 
-        k++; 
-        // Проверяем, содержит ли текущая строка минимальный элемент 
-        bool found = false; 
-        for (int j = 0; j < 4; j++) { 
-            if (matrix[i][j] == min) { 
-                found = true; 
-                break; 
-            } 
-        } 
-        // Если содержит, то вставляем новую строку 2, 4, 6, ... 
-        if (found) { 
-            for (int j = 0; j < 4; j++) { 
-                new_matrix[k][j] = (j + 1) * 2; 
-            } 
-            k++; 
-        } 
-    } 
-    // Присваиваем указателю на старую матрицу адрес новой матрицы 
-    matrix = new_matrix; 
-} 
- 
-// Главная функция программы 
-int main() { 
-    // Создаем матрицу 4x4 
-    glm::mat4 matrix; 
-    // Заполняем матрицу 
-    int choice; 
-    cout << "Выберите способ заполнения матрицы:" << endl; 
-    cout << "1 - случайными числами" << endl; 
-    cout << "2 - с клавиатуры" << endl; 
-    cin >> choice; 
-    switch (choice) { 
-        case 1: 
-        fill_random(matrix); 
-            break; 
-        case 2: 
-            fill_keyboard(matrix); 
-            break;
+#include <iostream>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+// Класс для работы с матрицами
+class Matrix {
+    // Поле для хранения элементов матрицы
+    std::vector<std::vector<int>> data;
+public:
+    // Конструктор, принимающий размеры матрицы
+    Matrix(int n, int m) {
+        // Инициализируем матрицу нулями
+        data.resize(n, std::vector<int>(m, 0));
+    }
+
+    // Метод для заполнения матрицы случайными числами
+    void fillRandom() {
+        // Создаем генератор случайных чисел
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(1, 100);
+
+        // Проходим по всем элементам матрицы
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < data[i].size(); j++) {
+                // Присваиваем элементу случайное значение
+                data[i][j] = dis(gen);
+            }
+        }
+    }
+
+    // Метод для заполнения матрицы с клавиатуры
+    void fillKeyboard() {
+        // Проходим по всем элементам матрицы
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < data[i].size(); j++) {
+                // Считываем элемент с клавиатуры
+                std::cin >> data[i][j];
+            }
+        }
+    }
+
+    // Метод для вывода матрицы на экран
+    void print() {
+        // Проходим по всем элементам матрицы
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < data[i].size(); j++) {
+                // Выводим элемент с пробелом
+                std::cout << data[i][j] << " ";
+            }
+            // Переходим на новую строку
+            std::cout << "\n";
+        }
+    }
+
+    // Метод для замены максимального элемента каждой строки номером столбца, в котором он находится
+    void replaceMax() {
+        // Проходим по всем строкам матрицы
+        for (int i = 0; i < data.size(); i++) {
+            // Находим индекс максимального элемента в строке
+            int maxIndex = std::max_element(data[i].begin(), data[i].end()) - data[i].begin();
+            // Заменяем максимальный элемент индексом
+            data[i][maxIndex] = maxIndex;
+        }
+    }
+
+    // Метод для вставки после всех строк, содержащих минимальный элемент массива, строки 2, 4, 6, ...
+    void insertRow() {
+        // Находим минимальный элемент во всей матрице
+        int min = data[0][0];
+        for (int i = 0; i < data.size(); i++) {
+            for (int j = 0; j < data[i].size(); j++) {
+                if (data[i][j] < min) {
+                    min = data[i][j];
+                }
+            }
+        }
+
+        // Создаем вектор для вставки
+        std::vector<int> row(data[0].size());
+        for (int i = 0; i < row.size(); i++) {
+            row[i] = (i + 1) * 2;
+        }
+
+        // Проходим по всем строкам матрицы в обратном порядке
+        for (int i = data.size() - 1; i >= 0; i--) {
+            // Проверяем, есть ли в строке минимальный элемент
+            bool hasMin = false;
+            for (int j = 0; j < data[i].size(); j++) {
+                if (data[i][j] == min) {
+                    hasMin = true;
+                    break;
+                }
+            }
+            // Если есть, то вставляем после нее вектор
+            if (hasMin) {
+                data.insert(data.begin() + i + 1, row);
+            }
+        }
+    }
+};
+
+int main() {
+    // Считываем размеры матрицы
+    int n, m;
+    std::cout << "Введите размеры матрицы: ";
+    std::cin >> n >> m;
+
+    // Создаем объект класса Matrix
+    Matrix matrix(n, m);
+
+    // Спрашиваем пользователя, как он хочет заполнить матрицу
+    char choice;
+    std::cout << "Как вы хотите заполнить матрицу? (r - случайными числами, k - с клавиатуры): ";
+    std::cin >> choice;
+
+    // Вызываем соответствующий метод
+    if (choice == 'r') {
+        matrix.fillRandom();
+    }
+    else if (choice == 'k') {
+        matrix.fillKeyboard();
+    }
+    else {
+        std::cout << "Неверный выбор\n";
+        return 0;
+    }
+
+    // Выводим исходную матрицу
+    std::cout << "Исходная матрица:\n";
+    matrix.print();// Вызываем метод для замены максимальных элементов
+    matrix.replaceMax();
+
+    // Выводим измененную матрицу
+    std::cout << "Матрица после замены максимальных элементов:\n";
+    matrix.print();
+
+    // Вызываем метод для вставки строк
+    matrix.insertRow();
+
+    // Выводим конечную матрицу
+    std::cout << "Матрица после вставки строк:\n";
+    matrix.print();
+
+    return 0;
+}
